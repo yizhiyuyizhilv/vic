f7ff5af33b69e949ede6fedc09fc0dfff346b049
wym <wym@wym-PC>
2018-04-07 19:57:13 +0800

    "NEW ENV"

--- src/main/java/com/vic/Application.java
+++ src/main/java/com/vic/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	//NEW ENV
 	public static void main(String[] args) throws Exception {
 		SpringApplication application = new SpringApplication(Application.class);
 		application.run(args);
